3e4ae3dbd2365ad6d2183c604cf83f8f5b690e43
Natalya <salander2015@mail.ru>
2022-12-03 22:59:52 +0300

    удален импорт в app

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { Route, Routes } from 'react-router-dom';
 import HomePage from './pages/Index/HomePage';
 import DefaultLayout from './components/layouts/DefaultLayouts';
 import FeedbackPage from './pages/FeedbackPage/FeedbackPage';
-import Registration from './pages/Registration/Registration';
 import ProductPage from './pages/Products/Products';
 import Basket from './pages/basket/basket';
 
@@ -13,7 +12,6 @@ function App() {
         <Route path={'/'} element={<DefaultLayout/>}>
         <Route index element={<HomePage/>} />
         <Route path={'feedbackpage'} element={<FeedbackPage/>} />
-        <Route path={'registration'} element={<Registration/>} />
         <Route path={'basket'} element={<Basket/>} />
         <Route path={'products'} element={<ProductPage/>}>
         <Route path={':userId'} element={<ProductPage/>}/>
